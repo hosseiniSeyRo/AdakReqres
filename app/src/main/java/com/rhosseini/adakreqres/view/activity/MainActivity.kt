c3f5c19e1f5558851a2cfd958a82143a86d5d018
r.h <zoho1381@gmail.com>
2019-11-08 17:02:07 +0330

    add menu to userDetails page

--- app/src/main/java/com/rhosseini/adakreqres/view/activity/MainActivity.kt
+++ app/src/main/java/com/rhosseini/adakreqres/view/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rhosseini.adakreqres.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.rhosseini.adakreqres.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 }
--- app/src/main/java/com/rhosseini/adakreqres/view/fragment/UserDetailsFragment.kt
+++ app/src/main/java/com/rhosseini/adakreqres/view/fragment/UserDetailsFragment.kt
@@ -3,6 +3,7 @@ package com.rhosseini.adakreqres.view.fragment
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -16,6 +17,10 @@ class UserDetailsFragment : Fragment() {
     private lateinit var binding: FragmentUserDetailsBinding
     private val safeArgs: UserDetailsFragmentArgs by navArgs()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,4 +36,16 @@ class UserDetailsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_user_details, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.actionEdit -> Toast.makeText(activity, "edit", Toast.LENGTH_SHORT).show()
+            R.id.actionDelete -> Toast.makeText(activity, "delete", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
