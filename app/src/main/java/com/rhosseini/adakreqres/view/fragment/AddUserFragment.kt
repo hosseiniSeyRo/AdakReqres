adab44ac219872005af5cd9cd024a7117afd6d44
r.h <zoho1381@gmail.com>
2019-11-08 17:28:31 +0330

    add menu to addUser page

--- app/src/main/java/com/rhosseini/adakreqres/view/fragment/AddUserFragment.kt
+++ app/src/main/java/com/rhosseini/adakreqres/view/fragment/AddUserFragment.kt
@@ -3,6 +3,7 @@ package com.rhosseini.adakreqres.view.fragment
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -15,6 +16,10 @@ class AddUserFragment : Fragment() {
     private lateinit var binding: FragmentAddUserBinding
     private val safeArgs: AddUserFragmentArgs by navArgs()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,5 +32,17 @@ class AddUserFragment : Fragment() {
         binding.etName.setText(safeArgs.user?.first_name)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_add_user, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.actionSave -> Toast.makeText(activity, "save", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
